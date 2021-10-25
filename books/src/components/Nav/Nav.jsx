354cbfccd772d207f8e06f9751226fb323e58de9
6akcbaHu <>
2021-10-25 14:53:42 +0300

    books

--- books/src/components/Nav/Nav.jsx
+++ books/src/components/Nav/Nav.jsx
@@ -1,122 +1,68 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import style from './Nav.module.scss'
 import {NavLink} from "react-router-dom";
 import * as axios from "axios";
 
 const initialState = {
     categories: ['all', 'art', 'biography', 'computers', 'history', 'medical', 'poetry'],
-
+    books: []
 };
 
+// let active;
+function reducer(state, action) {
 
-// // }
-// function  bookFetch(e) {
-// const [state,setState]=useState(e.target.innerHTML)
-//     // let books=e.target.innerHTML
-//
-//
-// }
-//  useEffect(() => {
-//     const getTask = async () => {
-//         const res = await axios.get("https://www.googleapis.com/books/v1/volumes?q=subject:History&key=AIzaSyDqGOZbu-wLQnXYT4Oa-gIcv8n5sqZCmDk")
-//             .then(res => {
-//                 return res
-//             })
-//         console.log(res)
-//     }
-//
-//
-//     getTask()
-// }, []);
-//         const getTask = async () => {
-//             const res = await axios.get("https://www.googleapis.com/books/v1/volumes?q=subject:History&key=AIzaSyDqGOZbu-wLQnXYT4Oa-gIcv8n5sqZCmDk")
-//                 .then(res => {
-//                     return res
-//                 })
-//             console.log(res)
+    switch (action.type) {
+        case 'art':
+      if(!state.books.map(v=>v.id).includes(action.id)){
+         // console.log(state.books.map(v=>v.id).includes(action.id))
+         //  console.log(action.id)
+          return {
+              ...state,
+              books:[...state.books,{
+                  id:action.id
+              }]
+          }
+      }
+      else {
 
-// fetch("https://www.googleapis.com/books/v1/volumes?q=subject:History&key=AIzaSyDqGOZbu-wLQnXYT4Oa-gIcv8n5sqZCmDk")
-//     .then((response) => {
-//         return response.json();
-//     })
-//     .then((data) => {
-//         console.log(data);
-//     });
-// axios.get("https://www.googleapis.com/books/v1/volumes?q=subject:History&key=AIzaSyDqGOZbu-wLQnXYT4Oa-gIcv8n5sqZCmDk")
-// .then(res=>console.log(res.data))
-// .then(data=>console.log(data))
-// }
-// const book=()=>{
-//     //  axios.get("https://www.googleapis.com/books/v1/volumes?q=subject:History&key=AIzaSyDqGOZbu-wLQnXYT4Oa-gIcv8n5sqZCmDk")
-//     // .then(res=>console.log(res))
-//     // .then(data=>console.log(data))
-//     const url = "https://www.googleapis.com/books/v1/volumes?q=subject:History&key=AIzaSyDqGOZbu-wLQnXYT4Oa-gIcv8n5sqZCmDk";
-//     fetch(url)
-//         .then(res=>console.log(res))
-//     .then(data=>console.log(data))
-// //www.googleapis.com/books/v1/volumes?q=subject:History&key=AIzaSyDqGOZbu-wLQnXYT4Oa-gIcv8n5sqZCmDk"))
-//
-// }
+          return {
+              ...state,
+              books: [...state.books]
+          }
+      }
+
+        case 'decrement':
+            return {count: state.count - 1};
+        default:
+            throw new Error();
+    }
+}
+
 const NavBars = (props) => {
-    const [state, setState] = useState('')
+    // const [state, setState] = useState('')
+    // const prevCountRef = useRef();
+    const [state, dispatch] = useReducer(reducer, initialState)
 
-    const [book, setBook] = useState(' ')
 
     function bookFetch(e) {
-        // if (state==='') {
-        setState(e.target.innerHTML)
-        if (state === '') {
-            // setState(e.target.innerHTML)
-            axios.get(`https://www.googleapis.com/books/v1/volumes?q=subject:${e.target.innerHTML}&key=AIzaSyDqGOZbu-wLQnXYT4Oa-gIcv8n5sqZCmDk`)
-                .then(res => console.log(res.data))
-            setBook(e.target.innerHTML)
-        }
-        if (state !== book && state !== '') {
-            axios.get(`https://www.googleapis.com/books/v1/volumes?q=subject:${state}&key=AIzaSyDqGOZbu-wLQnXYT4Oa-gIcv8n5sqZCmDk`)
-                .then(res => console.log(res.data))
-            setBook(e.target.innerHTML)
-        }
+        // e.target.setAttribute("disabled", 'true')
+        // console.log(e.target.getAttribute("disabled")==='true')
+        axios.get(`https://www.googleapis.com/books/v1/volumes?q=subject:${e.target.innerHTML}&maxResults=10&key=AIzaSyDqGOZbu-wLQnXYT4Oa-gIcv8n5sqZCmDk`)
+            .then(res => console.log(res.data.items.map(v => dispatch({
+                type: e.target.innerHTML,
+                id: v.id,
+                title: v.volumeInfo.title,
+                img: v.volumeInfo.imageLinks,
+                authors: v.volumeInfo.authors,
+                description: v.volumeInfo.description
 
-        // }
-        // else {
-        //     setState(prevState => console.log(prevState))
-        //         // ? prevState
-        //         // : e.target.innerHTML)
-        // }
-// console.log(book)
+            }))))
+
+
     }
 
-    // console.log(book)
-    // console.log(state)
-    // if(state) {
-    //     axios.get(`https://www.googleapis.com/books/v1/volumes?q=subject:${state}&key=AIzaSyDqGOZbu-wLQnXYT4Oa-gIcv8n5sqZCmDk`)
-    //         .then(res => console.log(res.data))
-    // }
-    // console.log(state)
-    // console.log(book)
-    // setBook(e.target.innerHTML)
-    // if (state==='') {
-    //     setState(e.target.innerHTML)
-    //     axios.get(`https://www.googleapis.com/books/v1/volumes?q=subject:${state}&key=AIzaSyDqGOZbu-wLQnXYT4Oa-gIcv8n5sqZCmDk`)
-    //         .then(res => console.log(res.data))
-    // } else {
-    //     setState(prevState => (prevState === e.target.innerHTML)
-    //         ? prevState
-    //         : e.target.innerHTML)
-    //     axios.get(`https://www.googleapis.com/books/v1/volumes?q=subject:${state}&key=AIzaSyDqGOZbu-wLQnXYT4Oa-gIcv8n5sqZCmDk`)
-    //         .then(res => console.log(res.data))
-    //     // setState(e.target.innerHTML)
-    //     // let books=e.target.innerHTML
-    //
-    //     // console.log(state)
-    // }
-    // }
+    console.log(state)
 
-    // if (state) {
-    //     axios.get(`https://www.googleapis.com/books/v1/volumes?q=subject:${state}&key=AIzaSyDqGOZbu-wLQnXYT4Oa-gIcv8n5sqZCmDk`)
-    //         .then(res => console.log(res.data))
-    // }
-    // console.log(state)
     return (
         <NavLink to={props.v} activeClassName={style.active}>
             <ul onClick={bookFetch}>
